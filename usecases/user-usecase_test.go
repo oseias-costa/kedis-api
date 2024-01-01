070f6c64de9fe706fc9fea1c45f0a7138dd4bb11
Oséias Costa <oseiasc2@gmail.com>
2024-01-01 11:14:00 -0300

    feat(user-usecase_test.go): create usecase subtests for: createUser, updateUser, getUserById and deleteUser

--- usecases/user-usecase_test.go
+++ usecases/user-usecase_test.go
@@ -5,17 +5,52 @@ import (
 	"testing"
 )
 
-func TestCreateUser(t *testing.T) {
+func TestUserUseCase(t *testing.T) {
+	var usecase = NewUserUseCase()
 	mockUser := entities.User{
+		ID:        "1",
 		FirstName: "Oséias",
 		LastName:  "Costa",
 		Age:       32,
 		Password:  "12345",
 	}
 
-	expect := "Oséias"
+	t.Run("Shold create a user", func(t *testing.T) {
+		t.Helper()
+		newuser, _ := usecase.CreateUserUseCase(mockUser)
+		expect := "Oséias"
 
-	if mockUser.FirstName != expect {
-		t.Errorf("Expected FirstName to be 'Oséias', got %s", mockUser.FirstName)
-	}
+		if newuser.FirstName != expect {
+			t.Errorf("Expected FirstName to be 'Oséias', got %s", mockUser.FirstName)
+		}
+	})
+
+	t.Run("Shold Get user by id", func(t *testing.T) {
+		t.Helper()
+		getUser, _ := usecase.GetUserUseCase(mockUser)
+		expect := 32
+
+		if getUser.Age != expect {
+			t.Errorf("Expect Age %v, but got %v", getUser.Age, expect)
+		}
+	})
+
+	t.Run("Shoud update user", func(t *testing.T) {
+		t.Helper()
+		updateUser, _ := usecase.UpdateUser(mockUser)
+		expect := "12345"
+
+		if updateUser.Password != expect {
+			t.Errorf("Expect password %s, but got %s", expect, updateUser.Password)
+		}
+	})
+
+	t.Run("Shoud delete user", func(t *testing.T) {
+		t.Helper()
+		deleteUser := usecase.DeleteUser(mockUser, "1")
+
+		if !deleteUser {
+			t.Error("The user is not ok to delete")
+		}
+	})
 }
