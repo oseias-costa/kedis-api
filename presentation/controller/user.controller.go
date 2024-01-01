9f91599bef9bfea536a361d2bf8e63cb170640e1
Oséias Costa <oseiasc2@gmail.com>
2023-12-31 21:16:42 -0300

    feat(user.controller.go): create method to delete user on controller

--- presentation/controller/user.controller.go
+++ presentation/controller/user.controller.go
@@ -14,6 +14,9 @@ var userRepository repository.UserRepository
 type UserController interface {
 	GetUserByID(w http.ResponseWriter, r *http.Request)
 	CreateNewUser(w http.ResponseWriter, r *http.Request)
+	GetAllUsers(w http.ResponseWriter, r *http.Request)
+	UpdateUser(w http.ResponseWriter, r *http.Request)
+	DeleteUser(w http.ResponseWriter, r *http.Request)
 }
 
 type userController struct{}
@@ -51,4 +54,44 @@ func (*userController) CreateNewUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(result)
+}
+
+func (*userController) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	users, err := userRepository.GetAllUsers()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message":"the list is empty"}`))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(users)
+}
+
+func (*userController) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var user entities.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error":"update user error decode json"}`))
+	}
+
+	result, errResult := userRepository.UpdateUser(user)
+	if errResult != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error":"update user error repository"}`))
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
+
+func (*userController) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	isDeleted := userRepository.DeleteUser(id)
+	if !isDeleted {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error":"user not find"}`))
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"response":"user deleted"}`))
 }
