3e533714dcbcc27d81661343e426abe15d5c112f
Oséias Costa <oseiasc2@gmail.com>
2024-01-02 19:17:39 +0000

    fix(entities/user.go): fixed change age to email

--- entities/user.go
+++ entities/user.go
@@ -4,6 +4,6 @@ type User struct {
 	ID        string `json:"id"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-	Age       int    `json:"age"`
+	Email     string `json:"email"`
 	Password  string `json:"password"`
 }
